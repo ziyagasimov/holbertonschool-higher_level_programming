f05102e8168bb5de28201ce6eab171964c800207
ziyagasimov <ziyagasimov0@gmail.com>
2025-10-25 18:17:10 +0000

    t7

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def islower(c):
+    """
+    Checks if a character c is lowercase.
+    Returns True if lowercase, False otherwise.
+    """
+    return ord('a') <= ord(c) <= ord('z')
