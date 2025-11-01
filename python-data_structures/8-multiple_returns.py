a73fd6112120750b75bc91d9944bc3e695b63304
ziyagasimiov <ziyagasimiov0@gmail.com>
2025-11-01 17:21:14 +0000

    t8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if sentence == "":
+def multiple_returns(senctence):
+    if senctence == "":
         return (0, None)
     else:
         return (len(senctence), senctence[0])
