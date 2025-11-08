472ad3facf7e46362626e708b398044f4ec7d39d
ziyagasimov <ziyagasimov0@gmail.com>
2025-11-08 18:46:22 +0000

    t3

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square class that defines a square by its size."""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize the square with a given size.
+
+        Args:
+            size (int): The size of the square (default is 0).
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
