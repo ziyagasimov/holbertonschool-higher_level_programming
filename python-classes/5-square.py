4615a1bfd78047c1cba9b95d3e9b55cf62874423
ziyagasimov <ziyagasimov0@gmail.com>
2025-11-08 18:49:52 +0000

    t5

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Square class that defines a square by its size."""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize the square with a given size."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the character #."""
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
