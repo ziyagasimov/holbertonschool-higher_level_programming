8e87777297dee0aa4ea0e374c8da7e6538f48e65
ziyagasimov <ziyagasimov0@gmail.com>
2025-11-08 18:44:06 +0000

    t1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square class that defines a square by its size."""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize the square with a given size."""
+        self.__size = size
