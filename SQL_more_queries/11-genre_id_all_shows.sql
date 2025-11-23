712454f42d8b4732cdbd6b51f7cc4cf8ee87fb2d
ziyagasimov <ziyagasimov0@gmail.com>
2025-11-23 20:15:16 +0400

    t11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all shows and their linked genre_id (NULL if none)
+-- Usage: cat 11-genre_id_all_shows.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
