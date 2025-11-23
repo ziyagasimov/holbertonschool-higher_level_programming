3b9dd3b8ece79e6683ee10ba27a54702407fe76d
ziyagasimov <ziyagasimov0@gmail.com>
2025-11-23 20:20:18 +0400

    t15

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Lists all Comedy shows
+-- Usage: cat 15-comedy_only.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
