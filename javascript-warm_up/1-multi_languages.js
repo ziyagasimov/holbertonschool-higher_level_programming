bb9a618e3cf399371c3658197cf11b9c81193e6d
ziyagasimov <ziyagasimov0@gmail.com>
2025-11-30 18:02:14 +0000

    t1

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,8 @@
-#!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+#!/usr/bin/env node
+const myVar = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+
+console.log(myVar);
+console.log(myVar2);
+console.log(myVar3);
