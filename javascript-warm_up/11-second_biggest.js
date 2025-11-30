2337b27c5f725b7d1dbf96a49af7c96f3a21b3fe
ziyagasimov <ziyagasimov0@gmail.com>
2025-11-30 18:28:15 +0000

    task

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const process = require('process');
+
+const args = process.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  args.sort((a, b) => b - a);
+  console.log(args[1]);
+}
