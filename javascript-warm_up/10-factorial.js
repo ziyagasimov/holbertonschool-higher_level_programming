acb3e31a305e21c4e1637d924faadfb75f5cdb68
ziyagasimov <ziyagasimov0@gmail.com>
2025-11-30 18:27:15 +0000

    task

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n) || n === 1 || n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(parseInt(process.argv[2])));
