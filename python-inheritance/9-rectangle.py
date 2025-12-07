26bc65451ce336980ee1aea2e1d1930f4207dd4c
ziyagasimov <ziyagasimov0@gmail.com>
2025-12-07 16:49:42 +0000

    t9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# 9-rectangle.py
+
+"""Defines a Rectangle class that inherits from BaseGeometry."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize a Rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the rectangle description for print() and str()."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
