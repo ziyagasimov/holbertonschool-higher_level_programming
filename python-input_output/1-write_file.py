7356496d77a4f3b43980eea0c5364465d64d6cec
ziyagasimov <ziyagasimov0@gmail.com>
2025-12-29 19:22:33 +0000

    t1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Bu modul mətnin fayla yazılması üçün funksiyanı ehtiva edir.
+write_file funksiyası verilən mətni fayla yazır və
+yazılmış simvolların sayını qaytarır.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Verilən mətni UTF-8 formatında fayla yazır.
+    Fayl yoxdursa yaradır, varsa məzmunu yenidən yazır.
+    Yazılmış simvolların sayını qaytarır.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
+        
